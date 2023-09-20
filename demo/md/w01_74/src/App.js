c45660ee1370ebb820d067d3225982db57a5afbe
CHEN211410674 <211410674@o365.tku.edu.tw>
2023-09-20 18:40:06 +0800

    ### W01-P4: Display three different <Book_xx />, and show third <Book_xx> in Chrome DevTools

--- demo/md/w01_74/src/App.js
+++ demo/md/w01_74/src/App.js
@@ -5,29 +5,26 @@ import Book_74 from './components/Book_74'
 
 const App = () => {
   return (
-    <section className="booklist">
+    <section className='booklist'>
         <Book_74
-           key='1'
-           img="https://m.media-amazon.com/images/I/81st0M5XcsL._SL1500_.jpg"
-           title="哈利·波特与魔法石"
-           author="J.K Rowling"
+           img='https://m.media-amazon.com/images/I/81st0M5XcsL._SL1500_.jpg'
+           title='哈利·波特与魔法石'
+           author='J.K Rowling'
         />
         
-        <Book
-          key='2'
-          img="https://m.media-amazon.com/images/I/81st0M5XcsL._SL1500_.jpg"
-           title="哈利·波特与魔法石"
-           author="J.K Rowling"
+        <Book_74
+           img='https://m.media-amazon.com/images/S/aplus-media-library-service-media/7a5f87cf-1a24-4fb9-b2e4-d041562e6d20.__CR0,0,300,300_PT0_SX300_V1___.jpg'
+           title='Guweiz: The Art of Gu Zheng Wei'
+           author='Gu Zheng Wei'
         />
-        <Book 
-           key='3'
-           img="https://m.media-amazon.com/images/I/81st0M5XcsL._SL1500_.jpg"
-            title="哈利·波特与魔法石"
-            author="J.K Rowling"
+        <Book_74 
+            img='https://m.media-amazon.com/images/I/81zsHFC+BML._SL1500_.jpg'
+            title='Leonardo da Vinci： The Graphic Work'
+            author=' Frank Zöllner'
         />
     </section>
   );
-}
+};
 
 const Book = () => {
   return (
--- w01_74/src/App.js
+++ w01_74/src/App.js
@@ -7,20 +7,23 @@ const App = () => {
   return (
     <section className='booklist'>
         <Book_74
+           key='1'
            img='https://m.media-amazon.com/images/I/81st0M5XcsL._SL1500_.jpg'
            title='哈利·波特与魔法石'
            author='J.K Rowling'
         />
         
         <Book_74
-           img='https://m.media-amazon.com/images/I/81st0M5XcsL._SL1500_.jpg'
-           title='哈利·波特与魔法石'
-           author='J.K Rowling'
+           key='2'
+           img='https://m.media-amazon.com/images/S/aplus-media-library-service-media/7a5f87cf-1a24-4fb9-b2e4-d041562e6d20.__CR0,0,300,300_PT0_SX300_V1___.jpg'
+           title='Guweiz: The Art of Gu Zheng Wei'
+           author='Gu Zheng Wei'
         />
         <Book_74 
-            img='https://m.media-amazon.com/images/I/81st0M5XcsL._SL1500_.jpg'
-            title='哈利·波特与魔法石'
-            author='J.K Rowling'
+            key='3'
+            img='https://m.media-amazon.com/images/I/81zsHFC+BML._SL1500_.jpg'
+            title='Leonardo da Vinci： The Graphic Work'
+            author=' Frank Zöllner'
         />
     </section>
   );
