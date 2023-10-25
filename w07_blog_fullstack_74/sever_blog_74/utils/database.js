74619e2e5ff708c5756bff6e77328b452c95d530
Chen211410674 <211410674@example.com>
2023-10-25 22:08:14 +0800

    w07

---
+++ w07_blog_fullstack_74/sever_blog_74/utils/database.js
@@ -0,0 +1,48 @@
+import pkg from 'pg';
+const {Pool} = pkg;
+
+//const { Pool } = require('pg');
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+let pool;
+if(process.env.DATABASE === 'SUPABASE'){
+    pool = new Pool({
+        host:process.env.SUPABASE_HOST,
+        port:'5432',
+        user:'postgres',
+        password: process.env.SUPABASE_PASSWORD,
+        database:'postgres',
+    });
+    console.log('supabase');
+}else{
+
+    pool = new Pool({
+        host:'localhost',
+        port:'5432',
+        user:'postgres',
+        password:'0000',
+        database:'wp1_demo_74',
+});
+console.log('local');
+}
+
+console.log('connect database', pool.options);
+
+// const testDB = async () => {
+//     try{
+//         const results = await pool.query(`select * from card_74`);
+//         console.log('json data',JSON.stringify( results.rows));
+//     }catch(error){
+//         console.log(error);
+//     }
+// }
+
+// testDB();
+
+console.log('connect database', pool.options);
+
+export default pool;
+
+//module.export = pool;
