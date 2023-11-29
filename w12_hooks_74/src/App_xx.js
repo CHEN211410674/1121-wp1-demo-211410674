8b44830d58b4e9153d0e7708746d693b06a5503c
Chen211410674 <211410674@example.com>
2023-11-29 22:00:28 +0800

    Add w12

---
+++ w12_hooks_74/src/App_xx.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import T13_74 from './tutorial/1-useState/T13-useState-array copy';
+// import T23_74 from './tutorial/2-useEffect/T23-useEffect-fetch-data';
+// import T21_74 from './tutorial/2-useEffect/T21-useEffect-basics ';
+import T14_74 from './tutorial/1-useState/T14-useState-object';
+
+const App_xx = () => {
+  return (
+    <div className='container'>
+      <h2>Tutorial T14_74</h2>
+      <T14_74 />
+    </div>
+  );
+};
+
+export default App_xx;
